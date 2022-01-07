13d55fd5e684ae9519e8faeffb36e62d95ea63d5
zxc12523 <b09902112@ntu.edu.tw>
2022-01-07 17:02:05 +0800

    123

---
+++ hw4/connect4_public/connect4.cpp
@@ -0,0 +1,15 @@
+#include "connect4.h"
+
+// The columns are numbered from 0 to 6
+int decide(int yp_move) {
+    if (yp_move == -1) {
+        // A new game starts
+        // TODO: Please remember to reset everything here (if needed)
+    }
+    else {
+        // YP placed his piece at column `yp_move` 
+
+    }
+
+    return 6; // always place at the rightmost column
+}
