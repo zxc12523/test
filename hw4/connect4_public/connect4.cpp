4476b7a896132d1d8236eaeaa6c479999e48187c
zxc12523 <b09902112@ntu.edu.tw>
2022-01-12 07:29:07 +0800

    p2

--- hw4/connect4_public/connect4.cpp
+++ hw4/connect4_public/connect4.cpp
@@ -17,7 +17,7 @@ using namespace std;
 long long op = 0;
 
 long long height[7];
-set<long long> valid;
+set<int> valid;
 
 long long grid[6][7];
 
@@ -36,8 +36,10 @@ void update_grid(long long p, long long col)
     grid[height[col]++][col] = p;
 }
 
-long long grade(map<long long, long long> &m, long long &score){
-    if (m[1] && m[2]){
+long long grade(map<long long, long long> m, long long score)
+{
+    if (m[1] && m[2])
+    {
         score += 0;
     }
     else if (m[1] == 4)
@@ -46,11 +48,11 @@ long long grade(map<long long, long long> &m, long long &score){
     }
     else if (m[1] == 3 && m[0] == 1)
     {
-        score += 10;
+        score += 32;
     }
     else if (m[1] == 2 && m[0] == 2)
     {
-        score += 5;
+        score += 8;
     }
     else if (m[1] == 1 && m[0] == 3)
     {
@@ -60,12 +62,38 @@ long long grade(map<long long, long long> &m, long long &score){
     {
         return -maxn;
     }
-    return 0;
+    else if (m[1] == 3 && m[0] == 1)
+    {
+        score -= 25;
+    }
+    else if (m[1] == 2 && m[0] == 2)
+    {
+        score -= 5;
+    }
+    else if (m[1] == 1 && m[0] == 3)
+    {
+        score -= 1;
+    }
+    return score;
 }
 
-long long grade_hor(map<long long, long long> &m, long long &reward){
-    for(long long i=0;i<6;i++) {
-        for(long long j=0;j<4;j++){
+long long grade_mid(long long reward)
+{
+    for (int i = 0; i < 6; i++)
+    {
+        if (grid[i][3] == 1)
+            reward += 3;
+    }
+    return reward;
+}
+
+long long grade_hor(long long reward)
+{
+    for (long long i = 0; i < 6; i++)
+    {
+        map<long long, long long> m;
+        for (long long j = 0; j < 4; j++)
+        {
             m[grid[i][j]]++;
         }
 
@@ -74,24 +102,33 @@ long long grade_hor(map<long long, long long> &m, long long &reward){
             return maxn;
         else if (tmp == -maxn)
             return -maxn;
+        else
+            reward = tmp;
 
-        for(long long j=4;j<7;j++){
+        for (long long j = 4; j < 7; j++)
+        {
             m[grid[i][j]]++;
-            m[grid[i][j-4]]--;
+            m[grid[i][j - 4]]--;
 
             long long tmp = grade(m, reward);
             if (tmp == maxn)
                 return maxn;
             else if (tmp == -maxn)
                 return -maxn;
+            else
+                reward = tmp;
         }
     }
-    return 0;
+    return reward;
 }
 
-long long grade_ver(map<long long, long long> &m, long long &reward){
-    for(long long j=0;j<7;j++) {
-        for(long long i=0;i<4;i++){
+long long grade_ver(long long reward)
+{
+    for (long long j = 0; j < 7; j++)
+    {
+        map<long long, long long> m;
+        for (long long i = 0; i < 4; i++)
+        {
             m[grid[i][j]]++;
         }
 
@@ -100,27 +137,36 @@ long long grade_ver(map<long long, long long> &m, long long &reward){
             return maxn;
         else if (tmp == -maxn)
             return -maxn;
+        else
+            reward = tmp;
 
-        for(long long i=4;i<6;i++){
+        for (long long i = 4; i < 6; i++)
+        {
             m[grid[i][j]]++;
-            m[grid[i-4][j]]--;
+            m[grid[i - 4][j]]--;
 
             long long tmp = grade(m, reward);
             if (tmp == maxn)
                 return maxn;
             else if (tmp == -maxn)
                 return -maxn;
+            else
+                reward = tmp;
         }
     }
-    return 0;
+    return reward;
 }
 
-long long grade_dia_l(long long &reward){
-    for(long long i=0;i<3;i++) {
-        for(long long j=0;j<4;j++){
+long long grade_dia_l(long long reward)
+{
+    for (long long i = 0; i < 3; i++)
+    {
+        for (long long j = 0; j < 4; j++)
+        {
             map<long long, long long> m;
-            for(long long k=0;k<4;k++){
-                m[grid[i+k][j+k]]++;
+            for (long long k = 0; k < 4; k++)
+            {
+                m[grid[i + k][j + k]]++;
             }
 
             long long tmp = grade(m, reward);
@@ -128,109 +174,144 @@ long long grade_dia_l(long long &reward){
                 return maxn;
             else if (tmp == -maxn)
                 return -maxn;
+            else
+                reward = tmp;
         }
     }
-    return 0;
+    return reward;
 }
 
-long long grade_dia_r(long long &reward){
-    for(long long i=0;i<3;i++) {
-        for(long long j=3;j<7;j++){
+long long grade_dia_r(long long reward)
+{
+    for (long long i = 0; i < 3; i++)
+    {
+        for (long long j = 3; j < 7; j++)
+        {
             map<long long, long long> m;
-            for(long long k=0;k<4;k++){
-                m[grid[i+k][j-k]]++;
+            for (long long k = 0; k < 4; k++)
+            {
+                m[grid[i + k][j - k]]++;
             }
-            
+
             long long tmp = grade(m, reward);
             if (tmp == maxn)
                 return maxn;
             else if (tmp == -maxn)
                 return -maxn;
+            else
+                reward = tmp;
         }
     }
-    return 0;
+    return reward;
 }
 
-long long evaluate(){
+long long evaluate()
+{
     long long reward = 0, d;
-    map<long long, long long> m;
 
-    d = grade_hor(m, reward);
+    reward = grade_mid(reward);
+
+    d = grade_hor(reward);
     if (d == maxn)
         return maxn;
     else if (d == -maxn)
         return -maxn;
-    m.clear();
+    else
+        reward = d;
 
-    d = grade_ver(m, reward);
+    d = grade_ver(reward);
     if (d == maxn)
         return maxn;
     else if (d == -maxn)
         return -maxn;
+    else
+        reward = d;
 
     d = grade_dia_l(reward);
     if (d == maxn)
         return maxn;
     else if (d == -maxn)
         return -maxn;
+    else
+        reward = d;
 
     d = grade_dia_r(reward);
     if (d == maxn)
         return maxn;
     else if (d == -maxn)
         return -maxn;
+    else
+        reward = d;
 
-    return 0;
+    return reward;
 }
 
-pii deter(long long forward_step, long long pre_min, long long pre_max, long long who){
-    check_valid();
+pii deter(long long forward_step, long long pre_min, long long pre_max, long long who, set<int> avilible)
+{
     long long e = evaluate();
-    if (e == maxn){
+    if (e == maxn)
+    {
         return {-1, e};
     }
-    else if (e == -maxn){
+    else if (e == -maxn)
+    {
         return {-1, e};
     }
-    else if (valid.size() == 0){
+    else if (avilible.size() == 0)
+    {
         return {-2, 0LL};
     }
-    else if (forward_step == 0){
+    else if (forward_step == 0)
+    {
         return {-2, e};
     }
-    else if (who == 1){
+    else if (who == 1)
+    {
         long long cur_ma = LONG_LONG_MIN, choose_col = -1;
-        for(auto j:valid){
+        for (auto j : avilible)
+        {
             grid[height[j]++][j] = 1;
-            pii p = deter(forward_step-1, pre_min, pre_max, 0);
+            set<int> tmp = avilible;
+            if (height[j] == 6)
+                tmp.erase(j);
+            pii p = deter(forward_step - 1, pre_min, pre_max, 0, tmp);
             grid[--height[j]][j] = 0;
 
-            if(p.second > cur_ma){
+            if (p.second > cur_ma)
+            {
                 cur_ma = p.second, choose_col = j;
             }
 
             pre_max = max(pre_max, cur_ma);
 
-            if (cur_ma > pre_min){
+            if (cur_ma > pre_min)
+            {
                 break;
             }
         }
         return {choose_col, cur_ma};
     }
-    else if (who == 0){
+    else if (who == 0)
+    {
         long long cur_mi = LONG_LONG_MAX, choose_col = -1;
-        for(auto j:valid){
+        for (auto j : avilible)
+        {
             grid[height[j]++][j] = 2;
-            pii p = deter(forward_step-1, pre_min, pre_max, 1);
+            set<int> tmp = avilible;
+            if (height[j] == 6)
+                tmp.erase(j);
+            pii p = deter(forward_step - 1, pre_min, pre_max, 1, tmp);
             grid[--height[j]][j] = 0;
 
-            if (p.second < cur_mi){
+            if (p.second < cur_mi)
+            {
                 cur_mi = p.second, choose_col = j;
             }
 
             pre_min = min(pre_min, cur_mi);
 
-            if (pre_max > cur_mi){
+            if (pre_max > cur_mi)
+            {
                 break;
             }
         }
@@ -259,14 +340,39 @@ int decide(int yp_move)
         update_grid(2LL, yp_move);
     }
 
-    if (op++ < 3)
+    if (op++ < 2)
     {
-        update_grid(1, 3);
-        return 3;
+        if (op == 1)
+        {
+            update_grid(1, 3);
+            return 3;
+        }
+        else
+        {
+            if (yp_move == 0 || yp_move == 3 || yp_move == 6)
+            {
+                update_grid(1, 3);
+                return 3;
+            }
+            else if (yp_move == 1 || yp_move == 4)
+            {
+                update_grid(1, 1);
+                return 1;
+            }
+            else if (yp_move == 2 || yp_move == 5)
+            {
+                update_grid(1, 5);
+                return 5;
+            }
+        }
     }
 
-    pii p = deter(5, LONG_LONG_MAX, LONG_LONG_MIN, 0);
+    check_valid();
+
+    pii p = deter(5, LONG_LONG_MAX, LONG_LONG_MIN, 1, valid);
     update_grid(1LL, p.first);
+
+    check_valid();
 
     return (int)p.first; // always place at the rightmost column
 }
